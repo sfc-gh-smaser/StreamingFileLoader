b969c0b810b6c4508cdb054427d877bb30df7271
Steven Maser <83292697+sfc-gh-smaser@users.noreply.github.com>
2023-03-14 22:18:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
- javac -cp lib/snowflake-ingest-sdk-1.0.2-beta.7.jar:lib/slf4j-api-2.0.6.jar:lib/slf4j-simple-2.0.6.jar:classes -d classes src/snowflake/demo/*.java -Xlint:unchecked
+ javac -cp lib/snowflake-ingest-sdk.jar:lib/slf4j-api-2.0.6.jar:lib/slf4j-simple-2.0.6.jar:classes -d classes src/snowflake/demo/*.java -Xlint:unchecked
 
  jar cfm StreamingFileLoader.jar manifest.txt -C classes snowflake src snowflake.properties sample1.csv
-
-
-
